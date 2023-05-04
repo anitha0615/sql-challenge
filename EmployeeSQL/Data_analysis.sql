8978bbeeadb2bba36146812f3ae187efea25e7f2
anitha0615 <125384475+anitha0615@users.noreply.github.com>
2023-05-04 19:18:27 -0400

    Update Data_analysis.sql

--- EmployeeSQL/Data_analysis.sql
+++ EmployeeSQL/Data_analysis.sql
@@ -5,7 +5,7 @@ e.last_name,
 e.first_name, e.sex, s.salary 
 from employees e left outer join 
 salaries s 
-on e.emp_no = s.emp_no
+on e.emp_no = s.emp_no;
 
 --List the first name, last name, and hire date for the employees who were hired in 1986.
 
@@ -13,7 +13,7 @@ select e.first_name,
 e.last_name, 
 e.hire_date
 from employees e 
-where date_part('year', e.hire_date) = '1986'
+where date_part('year', e.hire_date) = '1986';
 
 
 --List the manager of each department along with their department number, department name, 
@@ -31,7 +31,7 @@ from employees e ,
  from departments dep left outer join 
  dept_manager dm 
  on dep.dept_no = dm.dept_no ) d
- where e.emp_no = d.emp_no
+ where e.emp_no = d.emp_no;
 
 --List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
 
@@ -46,7 +46,7 @@ e.last_name,
  from employees e LEFT OUTER JOIN
  dept_emp d
 on e.emp_no = d.emp_no) b
-where dep.dept_no = b.dept_no
+where dep.dept_no = b.dept_no;
 
 
 --List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B
@@ -56,7 +56,7 @@ e.first_name,
 sex 
 from employees e
 where upper(trim(e.first_name)) = 'HERCULES'
-and trim(upper(e.last_name)) like 'B%'
+and trim(upper(e.last_name)) like 'B%';
 
 
 --List each employee in the Sales department, including their employee number, last name, and first name.
@@ -68,7 +68,7 @@ from employees e where e.emp_no in (select de.emp_no
 				from dept_emp de, 
 				departments d where 
 				de.dept_no = d.dept_no 
-				and trim(upper(d.dept_name)) = 'SALES')
+				and trim(upper(d.dept_name)) = 'SALES');
 
 
 --List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
@@ -84,10 +84,10 @@ from employees e,
 		departments d where 
 		de.dept_no = d.dept_no 
 		and trim(upper(d.dept_name)) in ('SALES','DEVELOPMENT') ) dept
-where e.emp_no = dept.emp_no	
+where e.emp_no = dept.emp_no;
 
 --List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
 
 select e.last_name, count(*) as frequency_count
 from employees e 
-group by e.last_name order by count(*) desc
+group by e.last_name order by count(*) desc;
